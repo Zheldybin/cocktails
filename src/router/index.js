e94a246a8f929cbd0d93769e2b692a7b438bf992
Zheldybin Anatoly <zheldybin.aa@mail.ru>
2024-04-14 15:25:51 +0300

    init

--- src/router/index.js
+++ src/router/index.js
@@ -6,15 +6,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('@/views/HomeView.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/recipe',
+      name: 'recipe',
+      component: () => import('@/views/RecipeView.vue')
+    },
+    {
+      path: '/random',
+      name: 'random',
+      component: () => import('@/views/RandomView.vue')
     }
   ]
 })
